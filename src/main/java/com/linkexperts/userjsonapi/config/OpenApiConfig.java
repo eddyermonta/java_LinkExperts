cb06819fa95ff58c9b118e9a17e0079c79b4a399
eddyermonta <eduardo.montano@utp.edu.co>
2024-09-18 14:11:49 -0500

    Documentacion api swagger

--- src/main/java/com/linkexperts/userjsonapi/config/OpenApiConfig.java
+++ src/main/java/com/linkexperts/userjsonapi/config/OpenApiConfig.java
@@ -1,8 +1,28 @@
 package com.linkexperts.userjsonapi.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui-custom.css")
+                .addResourceLocations("classpath:/static/custom-swagger-ui.css");
+    }
+
 
 }
---
+++ src/test/java/com/linkexperts/userjsonapi/UserControllerTest.java
@@ -0,0 +1,35 @@
+package com.linkexperts.userjsonapi;
+
+import com.linkexperts.userjsonapi.domain.dto.UserDTO;
+import com.linkexperts.userjsonapi.presentation.controller.impl.UserController;
+import com.linkexperts.userjsonapi.services.IUserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private IUserService userService;
+
+    @Test
+    void testGetUserById() {
+        UserDTO userDTO = new UserDTO("John Doe", "johndoe", "john@example.com", "123-456-7890", "johndoe.com");
+
+        when(userService.getUserById("1")).thenReturn(Mono.just(userDTO));
+
+        webTestClient.get().uri("/users/1")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(UserDTO.class).isEqualTo(userDTO);
+    }
+}
