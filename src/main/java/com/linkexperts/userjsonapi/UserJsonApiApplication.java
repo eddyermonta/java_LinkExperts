21b1aa6c4f347f81baaa56133ace74b028e3ed60
eddyermonta <eduardo.montano@utp.edu.co>
2024-09-18 02:09:54 -0500

    Commit inicial: Configuración del proyecto con dependencias esenciales
      - **Web**: `spring-boot-starter-web`
      - **Cache**: `spring-boot-starter-cache`
      - **WebFlux**: `spring-boot-starter-webflux`
      - **Swagger**: `io.swagger.core.v3:swagger-annotations`
      - **Lombok**: `org.projectlombok:lombok`
      - **DevTools**: `spring-boot-devtools`
      - **Pruebas**:
         - `spring-boot-starter-test`
         - `io.projectreactor:reactor-test`
         - `org.mockito:mockito-core
         `org.junit.jupiter:junit-jupiter-api`

---
+++ src/main/java/com/linkexperts/userjsonapi/UserJsonApiApplication.java
@@ -0,0 +1,13 @@
+package com.linkexperts.userjsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserJsonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserJsonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linkexperts/userjsonapi/UserJsonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkexperts.userjsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserJsonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
